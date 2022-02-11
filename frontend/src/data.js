34588e73f95295fb741ed71a5bd374006b16435a
MedOussamaBraiek <mohamed.braiek@esprit.tn>
2022-02-12 00:27:53 +0100

    my commit

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Great Summer Fit',
+            category: 'Shirt',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Pull & Bear',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Summer Fit',
+            category: 'Shirt',
+            image: '/images/p2.jpg',
+            price: 300,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 200,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Nike Summer Fit',
+            category: 'Shirt',
+            image: '/images/p3.jpg',
+            price: 250,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 100,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Lacosta Summer Fit',
+            category: 'Shirt',
+            image: '/images/p4.jpg',
+            price: 150,
+            brand: 'Lacosta',
+            rating: 4.5,
+            numReviews: 150,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'Nike Summer Fit',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 50,
+            description: 'high quality product'
+        },
+        {
+            _id: '6',
+            name: 'Puma Summer Fit',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 80,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+    ]
+}
+
+export default data;
